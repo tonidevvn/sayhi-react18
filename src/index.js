e8e98cb002b35248e4a406ef66875bc24a89b0e9
tonidevvn <tonidevvn@gmail.com>
2023-08-05 20:22:01 +0700

    apply scss for index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import "./index.css";
+import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./store/RootStore";
 import App from "./views/App";
